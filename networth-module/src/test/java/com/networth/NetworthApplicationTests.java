c0631a36f5700e8ffa29eea387b7a41353d8f044
soundar-surya <bittergourd76@gmail.com>
2021-06-01 15:28:11 +0530

    updated networth module

--- networth-module/src/test/java/com/networth/NetworthApplicationTests.java
+++ networth-module/src/test/java/com/networth/NetworthApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class H2databaseApplicationTests {
+class NetworthApplicationTests {
 
 	@Test
 	void contextLoads() {
