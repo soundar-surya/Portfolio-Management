f34192dd2b51ca2989c84919b775ff053fa87269
avanish singhal <avanishsinghal149@gmail.com>
2021-06-01 16:33:12 +0530

    Added dailyshareprices/repo

---
+++ dailyshareprices/bin/src/main/resources/schema.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS mutualfunddetails; 
+create table mutualfunddetails (mutualfund_id varchar(10) primary key,mutualfund_name varchar(50),mutualfund_value DOUBLE);
---
+++ dailyshareprices/src/main/java/com/cognizant/dailyshareprice/exception/StockNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cognizant.dailyshareprice.exception;
+
+public class StockNotFoundException extends Exception {
+
+	public StockNotFoundException(String string) {
+		super(string);
+	}
+
+}
