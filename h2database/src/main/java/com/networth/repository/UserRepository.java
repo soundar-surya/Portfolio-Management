af531e85dceaa55e5c8f89aa61d28c8c2ea73bae
soundar-surya <bittergourd76@gmail.com>
2021-06-01 15:25:11 +0530

    Updated networt module

--- h2database/src/main/java/com/networth/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.networth.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.networth.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{}
--- h2database/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS User;
-CREATE TABLE User (
-	id INT PRIMARY KEY,
-	username VARCHAR(50) NOT NULL,
-	password VARCHAR(50) NOT NULL
-);
-INSERT INTO User VALUES(1, 'surya', 'keychron');
-INSERT INTO User VALUES(2, 'jesse', 'bluemeth');
