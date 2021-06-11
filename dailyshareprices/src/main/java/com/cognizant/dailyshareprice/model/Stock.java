ec386d93d79c254200afb65da0adba8929af920a
soundar-surya <bittergourd76@gmail.com>
2021-06-11 08:00:07 +0530

    Added swagger ui

--- dailyshareprices/src/main/java/com/cognizant/dailyshareprice/model/Stock.java
+++ dailyshareprices/src/main/java/com/cognizant/dailyshareprice/model/Stock.java
@@ -1,10 +1,14 @@
 package com.cognizant.dailyshareprice.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@ApiModel(value="Stock Details", description = "A StockPrice Model")
 @Entity
 @Table(name="stockdetails")
 public class Stock {
@@ -12,12 +16,15 @@ public class Stock {
 
 	@Id
 	@Column(name="stock_id")
+	@ApiModelProperty(notes = "The database generated product ID")
 	private String stockId;
 	
 	@Column(name="stock_name")
+	@ApiModelProperty(notes = "The name of a stock", required = true)
 	private String stockName;
 	
 	@Column(name="stock_value")
+	@ApiModelProperty(notes = "The value of a stock", required = true)
 	private double stockValue;
 	
 	
