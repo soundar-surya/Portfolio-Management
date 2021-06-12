9335812c2b02e14d74085881c2d354b03eba0ae5
soundar-surya <bittergourd76@gmail.com>
2021-06-12 13:56:25 +0530

    test

---
+++ NetworthCalculation_dev/target/classes/data.sql
@@ -0,0 +1,12 @@
+insert into portfoliodetails values(101);
+insert into portfoliodetails values(102);
+
+insert into assetdetails values(1,101,'AXIS','MutualFund',10);
+insert into assetdetails values(2,101,'Baroda','MutualFund',6);
+insert into assetdetails values(3,101,'DEL','Share',5);
+insert into assetdetails values(4,101,'AMZ','Share',8);
+insert into assetdetails values(5,102,'DSP','MutualFund',5);
+insert into assetdetails values(6,102,'Edelweiss','MutualFund',10);
+insert into assetdetails values(7,102,'GGL','Share',8);
+insert into assetdetails values(8,102,'FFB','Share',6);
+
