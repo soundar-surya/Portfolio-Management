887520fdd63a43b27750c459f258b317cbf6f4d0
SwethaP-20 <79535163+SwethaP-20@users.noreply.github.com>
2021-06-16 08:43:38 +0530

    Delete dailyshareprice directory

--- dailyshareprice/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into stockdetails values ('DEL','Delloite',2500.0);
-insert into stockdetails values ('AMZ','Amazon',1500.0);
-insert into stockdetails values ('GGL','Google',1400.0);
-insert into stockdetails values ('FFB','FaceBook',2000.0);
