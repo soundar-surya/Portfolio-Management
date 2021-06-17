77562e2b6da34b2657a7df6144044d16d36e9553
SwethaP-20 <79535163+SwethaP-20@users.noreply.github.com>
2021-06-17 17:23:51 +0530

    Delete NetworthCalculation directory

--- NetworthCalculation/src/main/java/com/cognizant/repository/PortfolioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognizant.model.PortfolioDetails;
-
-public interface PortfolioRepository extends JpaRepository<PortfolioDetails, Integer> {
-
-}
